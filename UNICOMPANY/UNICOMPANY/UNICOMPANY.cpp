2893c77eac449747433655df8c29c4cf70a12f7b
M4R10M4R1N <140458500+M4R10M4R1N@users.noreply.github.com>
2023-07-27 16:16:34 -0600

    agrega proyecto

---
+++ UNICOMPANY/UNICOMPANY/UNICOMPANY.cpp
@@ -0,0 +1,71 @@
+// UNICOMPANY.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	cout << "+--------------------------------------------------------------------------------+ " << endl;
+	cout << "|				E M P L E A D O				         |" << endl;
+	cout << "+------+------------+-------------+-------------+------------------+-------------+" << endl;
+	cout << "|  ID  |   NOMBRE   |  A PATERNO  |  A MATERNO  |   F NACIMIENTO   |      RFC    |" << endl;
+	cout << "+------+------------+-------------+-------------+------------------+-------------+" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "|      |            |             |             |                  |             |" << endl;
+	cout << "+------+------------+-------------+-------------+------------------+-------------+\n" << endl;
+	cout << "+-----------------------------------------------+" << endl;
+	cout << "|		D I R E C T I V O		|" << endl;
+	cout << "+------+------------+---------------------------+" << endl;
+	cout << "|  ID  |    TIPO    | PRESTACION DE COMBISTIBLE |  " << endl;
+	cout << "+------+------------+---------------------------+" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "|      |            |                           |" << endl;
+	cout << "+------+------------+---------------------------+\n" << endl;
+	cout << "+-------------------------------------------------+" << endl;
+	cout << "|      C E N T R O   D E  T R A B A J O	          |" << endl;
+	cout << "+------+--------------+---------------------------+" << endl;
+	cout << "|  ID  |    PUESTO    | DESCRIPCION DEL PUESTO    |  " << endl;
+	cout << "+------+--------------+---------------------------+" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "|      |              |                           |" << endl;
+	cout << "+------+--------------+---------------------------+\n" << endl;
+	cout << "+-------------------------------------------------------------+" << endl;
+	cout << "|                D A T O S  D E L  C E N T R O                |" << endl;
+	cout << "+--------------------+-------------------------+--------------+" << endl;
+	cout << "|  NUMERO DE CENTRO  |    NOMBRE DEL CENTRO    |    CIUDAD    |  " << endl;
+	cout << "+--------------------+-------------------------+--------------+" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "|     		     |              	       |              |" << endl;
+	cout << "+--------------------+-------------------------+--------------+" << endl;
+
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
